ee215cdfd45b96c682682af97b2c0ef29b921f1e
luc1342as2 <98180767+luc1342as2@users.noreply.github.com>
2024-01-30 19:17:08 -0500

    Create Bank.c

---
+++ Bank.c
@@ -0,0 +1,40 @@
+//Lucas Ghigli
+//Bank Program
+//04/05/2023
+#include <stdio.h>
+int main() {
+ int accountNumber; //Account Number.
+ float beginningMonthBalance; //Beginning of the month Balance.
+ float totalCharges; //Monthly charges.
+ float totalCredits; //Monthly credits used.
+ float  allowedCreditLimit; //Credit limit amount.
+
+ while (1) {
+ printf("Enter account number: ");
+ scanf("%d", &accountNumber);
+ if (accountNumber == -1) {
+    break;
+                          }
+ printf("Enter beginning balance: ");
+ scanf("%f", &beginningMonthBalance);
+
+ printf("Enter total charges: ");
+ scanf("%f", &totalCharges);
+
+ printf("Enter total credits: ");
+ scanf("%f", &totalCredits);
+
+ printf("Enter credit limit: ");
+ scanf("%f", &allowedCreditLimit);
+
+ float newBalance =  beginningMonthBalance + totalCharges - totalCredits;
+
+ if (newBalance > allowedCreditLimit) {
+     printf("\nAccount Number: %d\n", accountNumber);
+     printf("\nCredit Limit: %f\n", allowedCreditLimit);
+     printf("New Balance: %.2f\n", newBalance);
+     printf("Credit Limit exceeded.\n");
+             }
+                           }
+return 0;
+           }
